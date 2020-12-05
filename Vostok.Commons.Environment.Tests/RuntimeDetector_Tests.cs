d526c8f81c1fa566b1faccb152232d1f31dbef07
iloktionov <iloktionov@skbkontur.ru>
2020-12-05 15:44:09 +0500

    RuntimeDetector: added detection of net5.0

--- Vostok.Commons.Environment.Tests/RuntimeDetector_Tests.cs
+++ Vostok.Commons.Environment.Tests/RuntimeDetector_Tests.cs
@@ -32,7 +32,35 @@ namespace Vostok.Commons.Environment.Tests
         public void Should_return_false_for_Core20_on_Core21() => RuntimeDetector.IsDotNetCore20.Should().BeFalse();
 
         [Test]
+        public void Should_return_false_for_Core30_on_Core21() => RuntimeDetector.IsDotNetCore30AndNewer.Should().BeFalse();
+
+        [Test]
+        public void Should_return_false_for_Core50_on_Core21() => RuntimeDetector.IsDotNet50AndNewer.Should().BeFalse();
+
+        [Test]
         public void Should_detect_Core21_and_newer() => RuntimeDetector.IsDotNetCore21AndNewer.Should().BeTrue();
+#endif
+
+#if NETCOREAPP3_1
+        [Test]
+        public void Should_return_false_for_Core50_on_Core31() => RuntimeDetector.IsDotNet50AndNewer.Should().BeFalse();
+
+        [Test]
+        public void Should_detect_Core21_and_newer() => RuntimeDetector.IsDotNetCore21AndNewer.Should().BeTrue();
+
+        [Test]
+        public void Should_detect_Core30_and_newer() => RuntimeDetector.IsDotNetCore30AndNewer.Should().BeTrue();
+#endif
+
+#if NET5_0
+        [Test]
+        public void Should_detect_Core21_and_newer() => RuntimeDetector.IsDotNetCore21AndNewer.Should().BeTrue();
+
+        [Test]
+        public void Should_detect_Core30_and_newer() => RuntimeDetector.IsDotNetCore30AndNewer.Should().BeTrue();
+
+        [Test]
+        public void Should_detect_Core50_and_newer() => RuntimeDetector.IsDotNet50AndNewer.Should().BeTrue();
 #endif
     }
 }
