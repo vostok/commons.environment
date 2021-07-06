3d01efde5d5cea3c1182b4a6cfb8b92fa1e692cc
Borovsky Arkady <borovskyav@list.ru>
2021-07-06 15:43:38 +0500

    Add commit time extraction methods

---
+++ Vostok.Commons.Environment/AssemblyCommitTimeExtractor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection;
+using JetBrains.Annotations;
+
+namespace Vostok.Commons.Environment
+{
+    [PublicAPI]
+    internal static class AssemblyCommitTimeExtractor
+    {
+        [CanBeNull]
+        public static DateTime? ExtractFromEntryAssembly()
+        {
+            try
+            {
+                return ExtractFromAssembly(Assembly.GetEntryAssembly());
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        [CanBeNull]
+        public static DateTime? ExtractFromAssembly(Assembly assembly)
+        {
+            try
+            {
+                var assemblyTitle = AssemblyTitleParser.GetAssemblyTitle(assembly);
+                return ExtractFromTitle(assemblyTitle);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        [CanBeNull]
+        public static DateTime? ExtractFromAssembly(string assemblyPath)
+        {
+            try
+            {
+                var version = AssemblyTitleParser.GetAssemblyFileVersion(assemblyPath);
+                return ExtractFromTitle(version?.FileDescription);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private static DateTime? ExtractFromTitle(string title)
+        {
+            return title == null ? null : AssemblyTitleParser.ParseCommitTime(title);
+        }
+    }
+}
